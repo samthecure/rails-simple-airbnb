db4ac362db1a145ee7a5048aff878e8bf1d36fc0
Samuel Magalhaes <samuel.f.magalhaes@gmail.com>
2024-01-28 20:21:01 +0100

    création des routes et seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,49 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Suppression des entrées
+Flat.destroy_all
+puts 'Supression des flats confirmé'
+
+Flat.create!(
+  name: 'Château',
+  address: '12 rue du château',
+  description: 'Forminadable château construit pour le baron de Lestag au 18e siècle.',
+  price_per_night: 320,
+  number_of_guests: 12
+)
+
+Flat.create!(
+  name: 'Villa Luxe',
+  address: '8 Avenue des Palmiers',
+  description: 'Belle villa de luxe avec vue sur la mer, piscine privée et jardin exotique.',
+  price_per_night: 500,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'Chalet Montagnard',
+  address: 'Les Alpages, Chalet 3',
+  description: 'Chalet en bois au pied des montagnes, parfait pour les amoureux de la nature.',
+  price_per_night: 250,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Appartement Cosy',
+  address: '25 Rue de la Charme',
+  description: 'Appartement confortable au cœur de la ville, idéal pour une escapade romantique.',
+  price_per_night: 120,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Loft Industriel',
+  address: '42 Rue des Artistes',
+  description: 'Spacieux loft avec un style industriel moderne, situé dans un quartier artistique.',
+  price_per_night: 180,
+  number_of_guests: 4
+)
+
+if Flat.create!
+puts 'La seed à bien été remplie'
+else
+puts 'Un problème est survenu lors de la création des entrées'
+end
